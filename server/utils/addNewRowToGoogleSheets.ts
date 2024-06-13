9f2beb0cd1c6dd40b536f191ba974d65674c7901
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-06-13 17:14:25 -0300

    Update addNewRowToGoogleSheets.ts

--- server/utils/addNewRowToGoogleSheets.ts
+++ server/utils/addNewRowToGoogleSheets.ts
@@ -16,15 +16,16 @@ const sheetsClient = sheets?.sheets({ version: "v4", auth });
  * @summary
  * Insert a new row into a spreadsheet.
  * Currently this is being used for SpreadSheet in:
- * https://docs.google.com/spreadsheets/d/1BDUgJ1WJufqXlFqyfBO-123/edit#gid=0
+ * https://docs.google.com/spreadsheets/d/1BDUgJ1WJufqXlFqyfBO-vpDx0IfRk2VauMNZzLYohRU/edit#gid=0
  *
  * @usage
  * ```js
  *   addNewRowToGoogleSheets({
  *         identityId: req?.query?.identityId,
  *         displayName: req?.query?.displayName,
- *         appName: "Race",
- *         event: "starts",
+ *         appName: "App Name Example",
+ *         event: "starts example event name",
+ *         urlSlug
  *       })
  *         .then()
  *         .catch();
@@ -44,8 +45,8 @@ export const addNewRowToGoogleSheets = async ({ identityId, displayName, usernam
     const dataRowToBeInsertedInGoogleSheets = [
       formattedDate,
       formattedTime,
-      identityId,
-      displayName || username,
+      sanitizeString(identityId),
+      sanitizeString(displayName) || username,
       appName,
       event,
       urlSlug,
@@ -64,3 +65,7 @@ export const addNewRowToGoogleSheets = async ({ identityId, displayName, usernam
     console.error(JSON.stringify(error));
   }
 };
+
+function sanitizeString(input) {
+  return input && input !== "null" ? input : "";
+}
