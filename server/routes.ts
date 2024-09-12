7ffbe66648bf6dc82c35d91278fd83fd1cb46dc2
Lina <linabell@mail.com>
2024-09-12 08:58:20 -0700

    update health check

--- server/routes.ts
+++ server/routes.ts
@@ -25,7 +25,7 @@ router.get("/system/health", (req, res) => {
   return res.json({
     appVersion: getVersion(),
     status: "OK",
-    deploymentDate: SERVER_START_DATE,
+    serverStartDate: SERVER_START_DATE,
     envs: {
       NODE_ENV: process.env.NODE_ENV,
       COMMIT_HASH: process.env.COMMIT_HASH ? process.env.COMMIT_HASH : "NOT SET",
